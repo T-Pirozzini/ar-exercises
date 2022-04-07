6756f931226e938361e31805ade04b1280f4d99a
vagrant <tpirozzini@gmail.com>
2022-04-07 02:54:23 +0000

    add exercise 5, 6, 7

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_store_revenue = Store.sum(:annual_revenue)
+puts "Total store revenue is #{@total_store_revenue}"
+
+@average_store_revenue = Store.average(:annual_revenue)
+puts "Average store revenue is #{@average_store_revenue}"
+
+@million_revenue_stores = Store.where("annual_revenue > 1000000").count
+puts "Number of stores with over 1 million revenue: #{@million_revenue_stores}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Travis", last_name: "Pirozzini", hourly_rate: 80)
+@store1.employees.create(first_name: "Chloe", last_name: "Boop", hourly_rate: 160)
+@store1.employees.create(first_name: "Bessie", last_name: "Beep", hourly_rate: 130)
+
+@store2.employees.create(first_name: "Ryu", last_name: "street_fighter", hourly_rate: 50)
+@store2.employees.create(first_name: "Link", last_name: "zelda", hourly_rate: 20)
+@store2.employees.create(first_name: "Luigi", last_name: "mario", hourly_rate: 10)
+@store2.employees.create(first_name: "Kirby", last_name: "dreamland", hourly_rate: 30)
